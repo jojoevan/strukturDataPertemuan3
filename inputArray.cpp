953f7ea1a88a7cc9673cf7f0c6b6578007c873b3
Jonathan <jonathanevanroestamadji@gmail.com>
2026-03-16 18:52:37 +0700

    Add files via upload

---
+++ inputArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai[5];
+
+    for(int i = 0; i < 5; i++) {
+        cout << "Masukkan nilai ke-" << i + 1 << " : ";
+        cin >> nilai[i];
+    }
+
+    cout << "\nData yang dimasukkan:" << endl;
+
+    for(int i = 0; i < 5; i++) {
+        cout << nilai[i] << endl;
+    }
+
+    return 0;
+}
