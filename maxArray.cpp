7928f73f35b92e8c1f5a9d0fd3a5236984cff8ec
Jonathan <jonathanevanroestamadji@gmail.com>
2026-03-16 19:07:07 +0700

    Add files via upload

---
+++ maxArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai[5];
+    int max;
+
+    for(int i = 0; i < 5; i++) {
+        cout << "Masukkan nilai ke-" << i + 1 << " : ";
+        cin >> nilai[i];
+    }
+
+    max = nilai[0];
+
+    for(int i = 0; i < 5; i++) {
+        if(nilai[i] > max) {
+            max = nilai[i];
+        }
+    }
+
+    cout << "Nilai maksimumnya adalah : " << max << endl;
+
+    return 0;
+}
